5c2e7b25857d8003f7a60f08bcda90b715d68978
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-24 12:10:59 -0300

    refac/ build script

--- build/build.sh
+++ build/build.sh
@@ -35,113 +35,173 @@ fi
 # -- Functions----------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-function cleanContainers() {
+function sanitize() {
+  docker system prune --volumes -f
 
-    container="$(docker ps -a | grep 'jupyterlab' | awk '{print $1}')"
-    docker stop "${container}"
-    docker rm "${container}"
+  for image_id in $(docker images --filter "dangling=true" -q --no-trunc); do
+    docker rmi $image_id 
+  done
+}
 
-    container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
+function cleanContainer() {
+    echo "Container $1 deletion"
+
+    container="$(docker ps -a | grep $1 | awk '{print $1}')"
+    
     while [ -n "${container}" ];
     do
       docker stop "${container}"
       docker rm "${container}"
-      container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
     done
+}
 
-    container="$(docker ps -a | grep 'spark-master' | awk '{print $1}')"
-    docker stop "${container}"
-    docker rm "${container}"
+function cleanContainers() {
+    declare -a container_names=( "jupyterlab" "spark-worker" "spark-master" "spark-base" "base" )
+    
+    for container_name in ${container_names[@]}; do
+      cleanContainer $container_name
+    done
+}
 
-    container="$(docker ps -a | grep 'spark-base' | awk '{print $1}')"
-    docker stop "${container}"
-    docker rm "${container}"
+function cleanImage() {
+  echo "Image *$1* deletion"
 
-    container="$(docker ps -a | grep 'base' | awk '{print $1}')"
-    docker stop "${container}"
-    docker rm "${container}"
-
+  for image_id in $(docker images | grep -m 1 $1 | awk '{print $3}'); do    
+    docker rmi -f $image_id
+    for subimages in $(docker images --filter since=$image_id -q); do
+      docker inspect --format='{{.Id}} {{.Parent}}' $subimages | cut -d' ' -f1 | cut -d: -f2 | xargs docker rmi -f
+    done
+  done
 }
 
 function cleanImages() {
 
     if [[ "${SHOULD_BUILD_JUPYTERLAB}" == "true" ]]
     then
-      docker rmi -f "$(docker images | grep -m 1 'jupyterlab' | awk '{print $3}')"
+      cleanImage 'jupyterlab'
     fi
 
     if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
     then
-      docker rmi -f "$(docker images | grep -m 1 'spark-worker' | awk '{print $3}')"
-      docker rmi -f "$(docker images | grep -m 1 'spark-master' | awk '{print $3}')"
-      docker rmi -f "$(docker images | grep -m 1 'spark-base' | awk '{print $3}')"
+      declare -a container_names=( "spark-worker", "spark-master", "spark-base" )
+    
+      for container_name in ${container_names[@]}; do
+        cleanImage $container_name
+      done
     fi
 
     if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
     then
-      docker rmi -f "$(docker images | grep -m 1 'base' | awk '{print $3}')"
+      cleanImage 'base'
     fi
 
 }
 
 function cleanVolume() {
-  docker volume rm "hadoop-distributed-file-system"
+  echo "Volume *$1* deletion"
+  docker volume rm $1
+}
+
+function cleanVolumes() {
+  declare -a volume_names=( "hadoop-distributed-file-system" )
+  
+  for volume_name in ${container_names[@]}; do
+    cleanVolume $volume_name
+  done
+  
+}
+
+function cleanEnvironment() {
+  sanitize;
+  cleanContainers;
+  cleanImages;
+  cleanVolumes;
+}
+
+function buildImage() {
+  build_args=$1
+  filename=$2
+  tag_name=$3
+
+  eval "docker build --force-rm --progress=plain $build_args -f $filename -t $tag_name ."
 }
 
 function buildImages() {
 
   if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
   then
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg scala_version="${SCALA_VERSION}" \
-      -f docker/base/Dockerfile \
-      -t base:latest .
+    build_arg_1="--build-arg build_date="${BUILD_DATE}"";
+    build_arg_2="--build-arg scala_version="${SCALA_VERSION}""
+    builds_args="$build_arg_1 $build_arg_2";
+    filename='docker/base/Dockerfile';
+    tag_name='base:latest';
+
+    buildImage "$builds_args" "$filename" "$tag_name"
   fi
 
   if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
   then
+    build_arg_1="--build-arg build_date="${BUILD_DATE}""
+    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
+    build_arg_3="--build-arg hadoop_version="${HADOOP_VERSION}""
+    builds_args="$build_arg_1 $build_arg_2 $build_arg_3";
+    
+    filename='docker/spark-base/Dockerfile';
+    tag_name="spark-base:${SPARK_VERSION}";
 
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg spark_version="${SPARK_VERSION}" \
-      --build-arg hadoop_version="${HADOOP_VERSION}" \
-      -f docker/spark-base/Dockerfile \
-      -t spark-base:${SPARK_VERSION} .
+    buildImage "$builds_args" "$filename" "$tag_name"
 
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg spark_version="${SPARK_VERSION}" \
-      -f docker/spark-master/Dockerfile \
-      -t spark-master:${SPARK_VERSION} .
+    build_arg_1="--build-arg build_date="${BUILD_DATE}"";
+    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
+    builds_args="$build_arg_1 $build_arg_2";
+    
+    filename='docker/spark-master/Dockerfile';
+    tag_name="spark-master:${SPARK_VERSION}";
 
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg spark_version="${SPARK_VERSION}" \
-      -f docker/spark-worker/Dockerfile \
-      -t spark-worker:${SPARK_VERSION} .
+    buildImage "$builds_args" "$filename" "$tag_name"
 
+    build_arg_1="--build-arg build_date="${BUILD_DATE}""
+    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
+    builds_args="$build_arg_1 $build_arg_2"; 
+    
+    filename='docker/spark-worker/Dockerfile';
+    tag_name="spark-worker:${SPARK_VERSION}";
+
+    buildImage "$builds_args" "$filename" "$tag_name"
   fi
 
   if [[ "${SHOULD_BUILD_JUPYTERLAB}" == "true" ]]
   then
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg scala_version="${SCALA_VERSION}" \
-      --build-arg spark_version="${SPARK_VERSION}" \
-      --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-      --build-arg scala_kernel_version="${SCALA_KERNEL_VERSION}" \
-      -f docker/jupyterlab/Dockerfile \
-      -t jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION} .
-  fi
+    build_arg_1="--build-arg build_date="${BUILD_DATE}"" 
+    build_arg_2="--build-arg scala_version="${SCALA_VERSION}"" 
+    build_arg_3="--build-arg spark_version="${SPARK_VERSION}""
+    build_arg_4="--build-arg jupyterlab_version="${JUPYTERLAB_VERSION}"" 
+    build_arg_5="--build-arg scala_kernel_version="${SCALA_KERNEL_VERSION}""
 
+    builds_args="$build_arg_1 $build_arg_2 $build_arg_3 $build_arg_4 $build_arg_5"; 
+    filename='docker/spark-worker/Dockerfile';
+    tag_name="jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION}";
+    
+    buildImage "$builds_args" "$filename" "$tag_name"
+  fi
+}
+
+function preamble() {
+  echo 'Dpkg::Progress-Fancy "1";' > /etc/apt/apt.conf.d/99progressbar
+}
+
+function buildEnvironment() {
+  preamble;
+  buildImages;
+}
+
+function prepareEnvironment() {
+  cleanEnvironment;
+  buildEnvironment;
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
 # -- Main --------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-cleanContainers;
-cleanImages;
-cleanVolume;
-buildImages;
+prepareEnvironment;
