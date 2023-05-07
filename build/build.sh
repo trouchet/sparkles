2f9bf4aab27b96e0fb7a3f1fef558747ccb9256c
Bruno Peixoto <brunolnetto@gmail.com>
2023-05-07 12:35:57 -0300

    refac/ improve documentation and functions in general

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-#
+#!/usr/bin/env bash
 # -- Build Apache Spark Standalone Cluster Docker Images
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -8,200 +7,252 @@
 
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
 
-SHOULD_BUILD_BASE="$(grep -m 1 build_base build.yml | grep -o -P '(?<=").*(?=")')"
-SHOULD_BUILD_SPARK="$(grep -m 1 build_spark build.yml | grep -o -P '(?<=").*(?=")')"
-SHOULD_BUILD_JUPYTERLAB="$(grep -m 1 build_jupyter build.yml | grep -o -P '(?<=").*(?=")')"
+# Description: Read the value of a key from the build configuration file (build.yml).
+# Inputs:
+#   $1: The key to search for in the build configuration file.
+# Outputs:
+#   The value corresponding to the provided key in the build configuration file.
+read_config() {
+  local key="$1"
+  local value=$(grep -m 1 "$key" build.yml | grep -o -P '(?<=").*(?=")')
 
-SPARK_VERSION="$(grep -m 1 spark build.yml | grep -o -P '(?<=").*(?=")')"
-JUPYTERLAB_VERSION="$(grep -m 1 jupyterlab build.yml | grep -o -P '(?<=").*(?=")')"
+  if [[ -z "$value" ]]; then
+    echo "Error: Required configuration value '$key' is missing in build.yml" >&2
+    exit 1
+  fi
+
+  echo "$value"
+}
+
+# Validate and assign build configuration values
+SHOULD_BUILD_BASE=$(read_config "build_base")               # Whether to build the base image
+SHOULD_BUILD_SPARK=$(read_config "build_spark")             # Whether to build the Spark images
+SHOULD_BUILD_JUPYTERLAB=$(read_config "build_jupyterlab")   # Whether to build the JupyterLab image
+SPARK_VERSION=$(read_config "spark")                        # Spark version
+JUPYTERLAB_VERSION=$(read_config "jupyterlab")              # JupyterLab version
+
 
 SPARK_VERSION_MAJOR=${SPARK_VERSION:0:1}
 
-if [[ "${SPARK_VERSION_MAJOR}" == "2" ]]
-then
+# Set Hadoop version and Scala version based on Spark version
+if [ "${SPARK_VERSION_MAJOR}" == "2" ]; then
+  # For Spark 2.x, use Hadoop 2.7, Scala 2.11, and Scala Kernel 0.6.0.
   HADOOP_VERSION="2.7"
   SCALA_VERSION="2.11.12"
   SCALA_KERNEL_VERSION="0.6.0"
-elif [[ "${SPARK_VERSION_MAJOR}"  == "3" ]]
-then
+elif [ "${SPARK_VERSION_MAJOR}" == "3" ]; then
+  # For Spark 3.x, use Hadoop 3.2, Scala 2.12, and Scala Kernel 0.10.9.
   HADOOP_VERSION="3.2"
   SCALA_VERSION="2.12.10"
   SCALA_KERNEL_VERSION="0.10.9"
 else
+  # Unsupported Spark version.
+  echo "Unsupported Spark version: $SPARK_VERSION"
   exit 1
 fi
 
+
 # ----------------------------------------------------------------------------------------------------------------------
-# -- Functions----------------------------------------------------------------------------------------------------------
+# -- Functions ----------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-function sanitize() {
+# Description: Clean up Docker environment
+# 
+# Inputs: None
+# Outputs: None
+sanitize() {
   docker system prune --volumes -f
 
   for image_id in $(docker images --filter "dangling=true" -q --no-trunc); do
-    docker rmi $image_id 
+    docker rmi "$image_id"
   done
 }
 
-function cleanContainer() {
-    echo "Container $1 deletion"
+# Description: Clean up a specific container
+# 
+# Inputs:
+#   $1: Container name
+# Outputs: None
+cleanContainer() {
+  local container_name="$1"
+  echo "Deleting container: $container_name"
 
-    container="$(docker ps -a | grep $1 | awk '{print $1}')"
-    
-    while [ -n "${container}" ];
-    do
-      docker stop "${container}"
-      docker rm "${container}"
-    done
+  # Loop until all containers with the given name are stopped and removed
+  while container=$(docker ps -a -q -f name="$container_name"); [[ -n "$container" ]]; do
+    docker stop "$container"  # Stop the container
+    docker rm "$container"    # Remove the container
+  done
 }
 
-function cleanContainers() {
-    declare -a container_names=( "jupyterlab" "spark-worker" "spark-master" "spark-base" "base" )
-    
-    for container_name in ${container_names[@]}; do
-      cleanContainer $container_name
-    done
+# Description: Clean up all containers
+# 
+# Inputs: None
+# Outputs: None
+cleanContainers() {
+  # Define an array of container names to be cleaned
+  local container_names=("jupyterlab" "spark-worker" "spark-master" "spark-base" "base")
+
+  # Loop over each container name and call the cleanContainer function
+  for container_name in "${container_names[@]}"; do
+    cleanContainer "$container_name"  # Clean the specific container
+  done
 }
 
-function cleanImage() {
+# Description: Delete the specified image and its associated sub-images.
+# 
+# Input: Image name or reference.
+# Output: None.
+function cleanImageAndSubimages() {
+
   echo "Image *$1* deletion"
 
-  for image_id in $(docker images | grep -m 1 $1 | awk '{print $3}'); do    
-    docker rmi -f $image_id
-    for subimages in $(docker images --filter since=$image_id -q); do
-      docker inspect --format='{{.Id}} {{.Parent}}' $subimages | cut -d' ' -f1 | cut -d: -f2 | xargs docker rmi -f
-    done
+  image_id=$(docker images --filter=reference="$1" -q)
+
+  if [[ -n "${image_id}" ]]; then
+    docker rmi -f "${image_id}"
+
+    subimages=$(docker images --filter=since="$image_id" -q)
+
+    if [[ -n "${subimages}" ]]; then
+      docker rmi -f "${subimages}"
+    fi
+  fi
+}
+
+# Description: Delete all relevant images based on the build configuration.
+#
+# Input: None.
+# Output: None.
+cleanImages() {
+
+  local container_names=("jupyterlab" "spark-worker" "spark-master" "spark-base")
+
+  for container_name in "${container_names[@]}"; do
+    cleanImageAndSubimages "${container_name}"
   done
 }
 
-function cleanImages() {
-
-    if [[ "${SHOULD_BUILD_JUPYTERLAB}" == "true" ]]
-    then
-      cleanImage 'jupyterlab'
-    fi
-
-    if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
-    then
-      declare -a container_names=( "spark-worker", "spark-master", "spark-base" )
-    
-      for container_name in ${container_names[@]}; do
-        cleanImage $container_name
-      done
-    fi
-
-    if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
-    then
-      cleanImage 'base'
-    fi
-
-}
-
+# Description: Clean up a specific Docker volume.
+#
+# Inputs:
+#   $1: Volume name
+# Outputs: None
 function cleanVolume() {
-  echo "Volume *$1* deletion"
-  docker volume rm $1
+  local volume_name="$1"
+  
+  # Check if the volume exists before removing it
+  if docker volume ls -q --filter name="$volume_name" | grep -q "$volume_name"; then
+    echo "Deleting volume: $volume_name"
+    docker volume rm "$volume_name"
+  else
+    echo "Volume '$volume_name' does not exist. Doing nothing."
+  fi
 }
 
+
+# Description: Clean up multiple Docker volumes.
+#
+# Inputs: None
+# Outputs: None
 function cleanVolumes() {
-  declare -a volume_names=( "hadoop-distributed-file-system" )
+  declare -a volume_names=("hadoop-distributed-file-system")
   
-  for volume_name in ${container_names[@]}; do
-    cleanVolume $volume_name
+  for volume_name in "${volume_names[@]}"; do
+    cleanVolume "$volume_name"
   done
-  
 }
 
-function cleanEnvironment() {
-  sanitize;
-  cleanContainers;
-  cleanImages;
-  cleanVolumes;
+# Description: Clean the Docker environment by sanitizing resources, cleaning containers, images, and volumes.
+#
+# Input: None.
+# Output: None.
+function cleanEnvironment() {  
+  # Step 1: Sanitize Docker resources
+  sanitize
+
+  # Step 2: Clean up containers
+  cleanContainers
+
+  # Step 3: Clean up images
+  cleanImages
+
+  # Step 4: Clean up volumes
+  cleanVolumes
 }
 
+# Description: Build a Docker image with the specified arguments.
+#
+# Inputs:
+#   $1: Dockerfile path
+#   $2: Tag name
+#   $3: Additional build arguments
+# Outputs: None
 function buildImage() {
-  build_args=$1
-  filename=$2
-  tag_name=$3
+  local dockerfile_path="$1"
+  local tag_name="$2"
+  local additional_args="$3"
 
-  eval "docker build --force-rm --progress=plain $build_args -f $filename -t $tag_name ."
+  docker build --build-arg build_date="${BUILD_DATE}" ${additional_args} \
+    -f "${dockerfile_path}" \
+    -t "${tag_name}" .
 }
 
+# Description: Build the Docker images based on the build configuration.
+#
+# Inputs: None
+# Outputs: None
 function buildImages() {
+  declare -A image_configs=(
+    ["base"]="docker/base/Dockerfile base:latest --build-arg scala_version=${SCALA_VERSION}"
+    ["spark-base"]="docker/spark-base/Dockerfile spark-base:${SPARK_VERSION} --build-arg spark_version=${SPARK_VERSION} --build-arg hadoop_version=${HADOOP_VERSION}"
+    ["spark-master"]="docker/spark-master/Dockerfile spark-master:${SPARK_VERSION} --build-arg spark_version=${SPARK_VERSION}"
+    ["spark-worker"]="docker/spark-worker/Dockerfile spark-worker:${SPARK_VERSION} --build-arg spark_version=${SPARK_VERSION}"
+    ["jupyterlab"]="docker/jupyterlab/Dockerfile jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION} --build-arg scala_version=${SCALA_VERSION} --build-arg spark_version=${SPARK_VERSION} --build-arg jupyterlab_version=${JUPYTERLAB_VERSION} --build-arg scala_kernel_version=${SCALA_KERNEL_VERSION}"
+  )
 
-  if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
-  then
-    build_arg_1="--build-arg build_date="${BUILD_DATE}"";
-    build_arg_2="--build-arg scala_version="${SCALA_VERSION}""
-    builds_args="$build_arg_1 $build_arg_2";
-    filename='docker/base/Dockerfile';
-    tag_name='base:latest';
-
-    buildImage "$builds_args" "$filename" "$tag_name"
-  fi
-
-  if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
-  then
-    build_arg_1="--build-arg build_date="${BUILD_DATE}""
-    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
-    build_arg_3="--build-arg hadoop_version="${HADOOP_VERSION}""
-    builds_args="$build_arg_1 $build_arg_2 $build_arg_3";
-    
-    filename='docker/spark-base/Dockerfile';
-    tag_name="spark-base:${SPARK_VERSION}";
-
-    buildImage "$builds_args" "$filename" "$tag_name"
-
-    build_arg_1="--build-arg build_date="${BUILD_DATE}"";
-    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
-    builds_args="$build_arg_1 $build_arg_2";
-    
-    filename='docker/spark-master/Dockerfile';
-    tag_name="spark-master:${SPARK_VERSION}";
-
-    buildImage "$builds_args" "$filename" "$tag_name"
-
-    build_arg_1="--build-arg build_date="${BUILD_DATE}""
-    build_arg_2="--build-arg spark_version="${SPARK_VERSION}""
-    builds_args="$build_arg_1 $build_arg_2"; 
-    
-    filename='docker/spark-worker/Dockerfile';
-    tag_name="spark-worker:${SPARK_VERSION}";
-
-    buildImage "$builds_args" "$filename" "$tag_name"
-  fi
-
-  if [[ "${SHOULD_BUILD_JUPYTERLAB}" == "true" ]]
-  then
-    build_arg_1="--build-arg build_date="${BUILD_DATE}"" 
-    build_arg_2="--build-arg scala_version="${SCALA_VERSION}"" 
-    build_arg_3="--build-arg spark_version="${SPARK_VERSION}""
-    build_arg_4="--build-arg jupyterlab_version="${JUPYTERLAB_VERSION}"" 
-    build_arg_5="--build-arg scala_kernel_version="${SCALA_KERNEL_VERSION}""
-
-    builds_args="$build_arg_1 $build_arg_2 $build_arg_3 $build_arg_4 $build_arg_5"; 
-    filename='docker/spark-worker/Dockerfile';
-    tag_name="jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION}";
-    
-    buildImage "$builds_args" "$filename" "$tag_name"
-  fi
+  for image_name in "${!image_configs[@]}"; do
+    read -r dockerfile_path tag_name additional_args <<<"${image_configs[$image_name]}"
+    buildImage "$dockerfile_path" "$tag_name" "$additional_args"
+  done
 }
 
+
+# Description: Sets up the APT progress bar for a fancier display during package installation.
+# 
+# Input: None.
+# Output: None.
 function preamble() {
+  # Enable fancy progress bar for package installation
   echo 'Dpkg::Progress-Fancy "1";' > /etc/apt/apt.conf.d/99progressbar
 }
 
+# Description: Builds the Docker images required for the Apache Spark standalone cluster environment.
+# 
+# Input: None.
+# Output: None.
 function buildEnvironment() {
-  preamble;
-  buildImages;
+  # Set up the APT progress bar
+  preamble
+
+  # Build the Docker images based on the build configuration
+  buildImages
 }
 
+# Description: Prepares the environment by cleaning up any existing containers, images, and volumes,
+# and then builds the Docker images required for the Apache Spark standalone cluster environment.
+# 
+# Input: None.
+# Output: None.
 function prepareEnvironment() {
-  cleanEnvironment;
-  buildEnvironment;
+  # Clean the Docker environment
+  cleanEnvironment
+
+  # Build the required Docker images
+  buildEnvironment
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
 # -- Main --------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
-prepareEnvironment;
+buildImages;
