98d8a19a486da607ba7da706b8aab0f470885145
Bruno Peixoto <brunolnetto@gmail.com>
2023-05-30 17:12:20 -0300

    feat/ scripts to push files to DockerHub

---
+++ scripts/docker-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Step 1: Get the container IDs of the desired containers
+container_ids=$(docker ps -a --filter "name=spark-" --format "{{.ID}}")
+
+# Step 2: Create a consolidated image
+consolidated_image="sparkles"
+for container_id in $container_ids; do
+    docker commit "$container_id" "$consolidated_image"
+done
+
+# Step 3: Login to DockerHub
+docker login
+
+# Step 4: Tag the consolidated image
+docker tag "$consolidated_image" "$1/$consolidated_image"
+
+# Step 5: Push the consolidated image to DockerHub
+docker push "$1/$consolidated_image"
