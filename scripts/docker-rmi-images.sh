8d2fac279ade96695b6326c7b5d5391853b61605
Bruno Peixoto <brunolnetto@gmail.com>
2023-06-03 18:44:17 -0300

    feat/ scritp for iamge removal

---
+++ scripts/docker-rmi-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# List of image names to remove
+images=(
+  "jupyterlab"
+  "spark-worker"
+  "spark-master"
+  "spark-base"
+  "base"
+)
+
+# Iterate over the images and remove them
+for image in "${images[@]}"; do
+  
+  # Get the image ID
+  image_id=$(docker images -q "$image")
+
+  # Check if the image exists
+  if [[ -n "$image_id" ]]; then
+    # Remove the image
+    docker rmi "$image_id"
+    echo "Removed image: $image"
+  else
+    echo "Image not found: $image"
+  fi
+done
